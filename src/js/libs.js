7b165fae7385d4820965577fdeba49265119a394
Will IM <vitalii.mieliet@gmail.com>
2025-05-16 20:19:18 +0300

    chore: Add axios library

--- src/js/libs.js
+++ src/js/libs.js
@@ -4,4 +4,6 @@ import 'izitoast/dist/css/iziToast.min.css';
 import Splide from '@splidejs/splide';
 import '@splidejs/splide/css';
 
-export { iziToast };
+import axios from 'axios';
+
+export { axios, iziToast, Splide };
