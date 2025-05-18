3ac80de9460d74b89fe0ff3aa0709b3a906fe2ec
Will IM <vitalii.mieliet@gmail.com>
2025-05-19 02:38:59 +0300

    Add modal-controll.js module

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { handleArtistsList } from './js/presenters/artist-list-presenter';
 import { handleRevies } from './js/presenters/feedback-presenter';
+import { closeModal, openModal } from './js/utils/modal-controller';
 import { handlArtistDetails } from './js/views/artist-details-view';
 
 document.addEventListener('DOMContentLoaded', function () {
   handleArtistsList();
   handleRevies();
   handlArtistDetails('65ada833af9f6d155db48eaa');
+  closeModal();
 });
