a4f6d10c81bda23e4a984a89cfb8d77aa268cfce
Will IM <vitalii.mieliet@gmail.com>
2025-05-20 19:35:27 +0300

    Revert "Merge pull request #35 from vitalii-mieliet/prokopiy-hero"
    This reverts commit 4ee9253939c7dbd79e6cdd38b1f03fdb10d090d5, reversing
    changes made to 298ab512cb8f400b21a1d3c2f3e4c775989434cb.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { handleArtistsList } from './js/presenters/artist-list-presenter';
 import { handleRevies } from './js/presenters/feedback-presenter';
 import './js/controllers/mobile-menu';
-import './js/artists-filter';
 
 document.addEventListener('DOMContentLoaded', function () {
   handleArtistsList();
