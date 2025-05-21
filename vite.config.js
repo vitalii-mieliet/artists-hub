3397a3659482097301947052f9cd9e95948afe4c
Will IM <vitalii.mieliet@gmail.com>
2025-05-21 06:36:04 +0300

    Add base: '/test-shop/'

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/test-shop/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
