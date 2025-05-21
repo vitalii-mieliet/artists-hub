cacc0e0921f533fef886a972f90b6e4b6ce892f0
Will IM <vitalii.mieliet@gmail.com>
2025-05-21 07:09:59 +0300

    Add inline svg

--- src/js/views/artist-list-view.js
+++ src/js/views/artist-list-view.js
@@ -36,9 +36,9 @@ export function renderArtists(artists) {
 >
   Learn More
   <span>
-    <svg class="learn-more-icon-caret-right" width="24" height="24">
-      <use href="/img/svg/sprite.svg#icon-caret-right"></use>
-    </svg>
+    <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M9 19.9426L17 12.9426L9 5.94263V19.9426Z" fill="white" />
+</svg>
   </span>
 </button>
         </li>`;
